e317ec09095f83651224a5c79775b5f338da84da
EliasDA <elias.delgado@avahitech.com>
2025-08-27 01:32:03 -0600

    stremlit changes v1

--- src/code/config.py
+++ src/code/config.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config:
-    SERVER_ADDRESS = "3.125.95.236:8188"  #os.getenv("MODEL_ID")
-    WORKFLOW_FILENAME = "boomio-testing-API.json"#os.getenv("TABLE_NAME")
+    SERVER_ADDRESS = "3.125.95.236:8188"
+    WORKFLOW_FILENAME = r"src\boomio-testing-API.json"
