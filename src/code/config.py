f19b19fa3900ad09f132cd9ceace648d53536dc9
EliasDA <elias.delgado@avahitech.com>
2025-08-28 21:20:15 -0600

    Update config.py

--- src/code/config.py
+++ src/code/config.py
@@ -3,6 +3,6 @@ import os
 
 class Config:
     SERVER_ADDRESS = "3.125.95.236:8188"  #os.getenv("MODEL_ID")
-    WORKFLOW_CHARACTER = "scr/character-design-workflow.json"#os.getenv("TABLE_NAME")
-    WORKFLOW_OBSTACLE = "scr/obstacle-design-workflow.json"#os.getenv("TABLE_NAME")
-    WORKFLOW_BACKGROUND = "scr/background-design-workflow.json"#os.getenv("TABLE_NAME")
+    WORKFLOW_CHARACTER = "src/character-design-workflow.json"#os.getenv("TABLE_NAME")
+    WORKFLOW_OBSTACLE = "src/obstacle-design-workflow.json"#os.getenv("TABLE_NAME")
+    WORKFLOW_BACKGROUND = "src/background-design-workflow.json"#os.getenv("TABLE_NAME")
