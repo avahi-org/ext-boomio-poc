74fac6cd09bf2c82d0d4d50037de373029112250
EliasDA <elias.delgado@avahitech.com>
2025-09-15 16:45:38 -0600

    Update 2_✨Brandbook asset generator.py

--- src/pages/2_✨Brandbook asset generator.py
+++ src/pages/2_✨Brandbook asset generator.py
@@ -215,7 +215,7 @@ with st.container():
                 if workflow_obstacle:
                     with st.spinner("Processing..."):
                         # Update the workflow with a random seed
-                        workflow_obstacle = wrk.update_workflow_with_rdn_seed(workflow_obstacle, "2")
+                        workflow_obstacle = wrk.update_workflow_with_rdn_seed(workflow_obstacle, "5")
 
                         # Update the workflow with a new prompt
                         workflow_obstacle = wrk.update_workflow_with_prompt(workflow_obstacle, prompt_obstacles, "3")
